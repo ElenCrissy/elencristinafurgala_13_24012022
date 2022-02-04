7dae8cd230e7bb0ce3c28e7b7fc33301798b31f1
Elencrissy <cristina.furgala@gmail.com>
2022-02-04 17:02:58 +0100

    Store

--- src/redux/store.js
+++ src/redux/store.js
@@ -18,6 +18,27 @@ const initialState = {
 export const authenticateUser = userInput => dispatch => {
     // dispatch in progress
     // appel
+    const url = 'http://localhost:3001/api/v1/user'
+    const init = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        // body: JSON.stringify({
+        //     email: email,
+        //     password: password,
+        // })
+    }
+    fetch(url, init)
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+
+    }
     // dispatch success et stock JWT ou error
 }
 
